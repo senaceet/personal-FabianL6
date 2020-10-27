7f34dee5de353fbd0e135e9ded0aa4b057bcf545
FabianL6 <falopez736@misena.edu.co>
2020-10-27 17:10:22 -0500

    shore: project fruteria

---
+++ 03 Desarrollo/laravel/fruteria/resources/views/landing/contacto.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-md-10 mx-auto">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63625.49629954472!2d-74.17036804931041!3d4.66633256076585!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9a47b1bd17ed%3A0x7c8f4ede734f52f3!2sFRUTERIA%20Y%20CAFETERIA%20LA%2062!5e0!3m2!1ses!2sco!4v1603834035556!5m2!1ses!2sco" width="1000" height="650" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </div>
+    </div>
+
+
+
+        <div class="row">
+            <div class="col-md-12 bg-dark p-4 mt-5">
+                <div class="row">
+
+                    <h2 class="text-white ml-5 mx-auto my-auto" >CONTACTO</h2> <img class="w-25 mx-auto" src="https://comefruta.es/wp-content/uploads/logo-horizontal-mefruta-saborysalud.png" alt=""><br>
+
+                </div>
+
+                <h2 class="text-white mt-4 ml-3"><b>ComeFruta</b></h2><br>
+
+                <h3 class="text-white ml-3"><b>Calle 62 #11-49</b></h3><br>
+                <h3 class="text-white ml-3 "><b>Telefono: 8124567</b></h3>
+
+            </div>
+    </div>
+
+
+
+    @endsection
